4d2c820a651fc27feecb30e5fdc57e0979ff895e
Artem Govorov <artem.govorov@gmail.com>
2015-12-30 12:44:34 +1000

    Fixed wallaby config

--- app/components/app/app.spec.ts
+++ app/components/app/app.spec.ts
@@ -1,8 +1,8 @@
-// import {AppComponent} from './app';
+import {AppComponent} from './app';
 
 describe('greeting', () => {
-    
+
     it('should greet', () => {
-        expect('Hello, John').toBe('Hello, John');  
+        expect('Hello, John').toBe('Hello, John');
     })
 })
