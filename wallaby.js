ccba0ad267a0c054c03e6125a1ea1dc7799ae055
Artem Govorov <artem.govorov@gmail.com>
2016-01-27 11:24:15 +1000

    Added promise catch in wallaby config

--- wallaby.js
+++ wallaby.js
@@ -60,7 +60,7 @@ module.exports = function (wallaby) {
 
             Promise.all(promises).then(function () {
                 wallaby.start();
-            });
+            }).catch(function (e) { setTimeout(function (){ throw e; }, 0); });
         }
     };
 };
